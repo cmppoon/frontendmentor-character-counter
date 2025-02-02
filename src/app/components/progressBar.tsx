6d511f3fb600d9ecc55fe69f12beb5754065ad7a
Chaiyapruek Muangsiri <cmp.poon@gmail.com>
2025-02-02 18:25:16 +0800

    fix progress bar dark theme

--- src/app/components/progressBar.tsx
+++ src/app/components/progressBar.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function ProgressBar({ progress }: { progress: number }) {
   return (
-    <div className="w-full bg-neutral-100 rounded-[30px] h-4 dark:bg-gray-700">
+    <div className="w-full bg-neutral-100 rounded-[30px] h-4 dark:bg-neutral-800">
       <div
-        className="bg-purple-400 h-4 rounded-[30px] dark:bg-blue-500"
+        className="bg-purple-400 h-4 rounded-[30px]"
         style={{width: progress + "%"}}
       ></div>
     </div>
